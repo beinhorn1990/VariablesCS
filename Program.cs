74b41a99b7b1700b8fc5f12a55e0748a5213a23a
Brendan Einhorn <beinhorn90@gmail.com>
2021-01-12 18:42:03 -0500

    partially completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace VariablesCS
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var numberOfCupsOfCoffee= 1;
+var fullName = "Brendan Einhorn";
+var today = DateTime.Now;
+Console.Write("$numberOfCupsOfCoffee" ); Console.Write("$fullname" ); Console.Write("$today");
+// var username = Console.Write("What is your name? ");
+// Console.WriteLine($"It is a pleasure to meet you, {username}");
+// var firstnumberasstring = Console.WriteLine($"Please input one number");
+// var secondnumberasstring = Console.WriteLine($"Please input a second number");
+// string[] numbers = {"$Firstnumberasstring", "$secondnumberasstring"};
+
+
     }
-  }
-}
+  }}
+
